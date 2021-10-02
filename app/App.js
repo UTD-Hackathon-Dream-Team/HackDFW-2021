c76235402934442af34bce6e4bd189593f8e5404
Reshmi Ranjith <rreshmiranjith@gmail.com>
2021-10-02 16:00:18 -0500

    add base nav

--- app/App.js
+++ app/App.js
@@ -1,13 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Login from "./screens/Login";
+import Daily from "./screens/Daily";
+import Facts from "./screens/Facts";
+import Photo from "./screens/Photo";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Home() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Daily" component={Daily} />
+      <Tab.Screen name="Photo" component={Photo} />
+      <Tab.Screen name="Facts" component={Facts} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="LogIn" component={Login} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ app/screens/Facts.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+function Facts() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Login Screen</Text>
+      </View>
+    );
+}
+
+export default Facts;
