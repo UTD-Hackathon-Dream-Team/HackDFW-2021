3a08203ed6e209ca87d954db8363697ad8b68d75
medhajon <msj170000@utdallas.edu>
2021-10-02 19:17:54 -0400

    TestPhotoPage
    Will do rows later

--- app/screens/Photo.js
+++ app/screens/Photo.js
@@ -1,12 +1,46 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { 
+  View, 
+  Image,
+  Text,
+  StyleSheet,
+  Dimensions, } from 'react-native';
+
+  const styles = StyleSheet.create({
+    selectImage: {
+      width: 120,
+      height: 120,
+      flexDirection: "row",
+      justifyContent: "space-between",
+      flexWrap: "wrap",
+    },
+  });
 
 function Photo() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View>
+        <View>
         <Text>Login Screen</Text>
+        <Image
+          style = {styles.selectImage}
+          source={require('../TestImages/Honey.png')}
+        />     
+        <Image
+          style = {styles.selectImage}
+          source={require('../TestImages/tang.jpg')}
+        />    
+        <Image
+          style = {styles.selectImage}
+          source={require('../TestImages/Honey.png')}
+        />  
+         <Image
+          style = {styles.selectImage}
+          source={require('../TestImages/tang.jpg')}
+        /> 
+        </View>
       </View>
+      
+      
     );
 }
-
 export default Photo;
