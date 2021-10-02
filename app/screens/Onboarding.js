5f49e29122358f104689bc416d048430d16cdbd8
SaloniSS <sxs170015@utdallas.edu>
2021-10-02 16:52:40 -0500

    selecting name and plan

--- app/screens/Onboarding.js
+++ app/screens/Onboarding.js
@@ -1,12 +1,53 @@
 import * as React from "react";
-import { View, Text, Button } from "react-native";
+import {
+  Input,
+  Center,
+  NativeBaseProvider,
+  Text,
+  Button,
+  Radio,
+} from "native-base";
 
 function Onboarding({ navigation }) {
+  const [plan, setPlan] = React.useState("configured");
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Login Screen</Text>
-      <Button title="Done" onPress={() => navigation.navigate("Home")} />
-    </View>
+    <NativeBaseProvider>
+      <Center flex={1} px="3">
+        {/* Patient Name */}
+        <Text>Adventurer Name</Text>
+        <Input
+          size="md"
+          placeholder="Name"
+          w={{
+            base: "70%",
+            md: "20%",
+          }}
+        />
+        {/* Adventure Plan */}
+        <Text>Adventure Plan: </Text>
+        <Radio.Group
+          value={plan}
+          onChange={(newPlan) => {
+            setPlan(newPlan);
+          }}
+        >
+          <Radio value="configured" my={1}>
+            Pre-Configured Plan
+          </Radio>
+          <Radio value="custom" my={1}>
+            Custom Plan
+          </Radio>
+        </Radio.Group>
+
+        {plan === "configured" ? (
+          <Text> Configured Plan </Text>
+        ) : (
+          <Text> Custom Plan </Text>
+        )}
+        <Button onPress={() => navigation.navigate("Home")}> Done </Button>
+      </Center>
+    </NativeBaseProvider>
   );
 }
 
