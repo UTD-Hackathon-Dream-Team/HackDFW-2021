ab76569d9479b20b19614f4c2c468d050195b2f3
meganjtran <meganjtran@gmail.com>
2021-10-02 18:10:18 -0500

    ugly swiper no swiping
    why does expo hate me :(

--- app/screens/Login.js
+++ app/screens/Login.js
@@ -1,16 +1,48 @@
 import * as React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Image } from "react-native";
 import Onboarding from "./Onboarding";
+import Swiper from "react-native-swiper";
+
+import book from "../assets/book.png";
+
+var styles = {
+  slides: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#7a3d14",
+  },
+  desc: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  icon: {
+    width: 350,
+    height: 400,
+  },
+}
 
 function Login({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Login Screen</Text>
-      <Button
+    <Swiper showsButtons loop={false}>
+      <View style={styles.slides}>
+      <Image source={book} style={styles.icon}></Image>
+        <Text style={styles.desc}>
+          Welcome to Health Quest!
+        </Text>
+      </View>
+      <View style={styles.slides}>
+      <Image source={book} style={styles.icon}></Image>
+        <Text style={styles.desc}>
+          Track daily tasks and learn fun facts on your quest to stay healthy!
+          </Text>
+          <Button
         title="Go to Details"
-        onPress={() => navigation.navigate("Onboarding")}
-      />
-    </View>
+        onPress={() => navigation.navigate('Home')}/>
+      </View>
+    </Swiper>
   );
 }
 
