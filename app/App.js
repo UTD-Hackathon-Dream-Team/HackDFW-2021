0a57e883ef682800e2f090f8d6f61fa45faf8771
Reshmi Ranjith <rreshmiranjith@gmail.com>
2021-10-03 09:33:26 -0500

    ignore warnings

--- app/App.js
+++ app/App.js
@@ -15,6 +15,7 @@ import { FontAwesome } from "@expo/vector-icons";
 import { Fontisto } from "@expo/vector-icons";
 import { LogBox, Text } from "react-native";
 
+LogBox.ignoreAllLogs(true);
 LogBox.ignoreLogs(["Warning:"]);
 
 const Stack = createNativeStackNavigator();
