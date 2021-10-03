772464cfc523cbb254032eff02966fb2de081ff3
Reshmi Ranjith <rreshmiranjith@gmail.com>
2021-10-02 21:44:48 -0500

    add ar

--- app/screens/Photo.js
+++ app/screens/Photo.js
@@ -1,46 +1,56 @@
-import * as React from 'react';
-import { 
-  View, 
-  Image,
-  Text,
-  StyleSheet,
-  Dimensions, } from 'react-native';
+import * as React from "react";
+import { Image, StyleSheet, Dimensions, TouchableOpacity } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import { Text, Heading, VStack, HStack, ScrollView } from "native-base";
 
-  const styles = StyleSheet.create({
-    selectImage: {
-      width: 120,
-      height: 120,
-      flexDirection: "row",
-      justifyContent: "space-between",
-      flexWrap: "wrap",
-    },
-  });
+const styles = StyleSheet.create({
+  arImage: {
+    width: Dimensions.get("screen").width / 2,
+    height: Dimensions.get("screen").width / 2,
+    margin: 10,
+  },
+});
 
 function Photo() {
-    return (
-      <View>
-        <View>
-        <Text>Login Screen</Text>
-        <Image
-          style = {styles.selectImage}
-          source={require('../TestImages/Honey.png')}
-        />     
-        <Image
-          style = {styles.selectImage}
-          source={require('../TestImages/tang.jpg')}
-        />    
-        <Image
-          style = {styles.selectImage}
-          source={require('../TestImages/Honey.png')}
-        />  
-         <Image
-          style = {styles.selectImage}
-          source={require('../TestImages/tang.jpg')}
-        /> 
-        </View>
-      </View>
-      
-      
-    );
+  return (
+    <ScrollView
+      style={{
+        padding: 10,
+      }}
+    >
+      <Heading style={{ alignSelf: "center" }}>Adventurer Buddies</Heading>
+      <VStack style={{ alignSelf: "center" }}>
+        <HStack>
+          <TouchableOpacity
+            onPress={() =>
+              WebBrowser.openBrowserAsync(
+                "https://api.echo3d.co/webar?key=quiet-pond-7565&entry=6b674fdd-db79-409a-8afc-86f4a0ea5f60"
+              )
+            }
+          >
+            <Image
+              source={require("../TestImages/Honey.png")}
+              style={styles.arImage}
+            />
+          </TouchableOpacity>
+          <Image
+            source={require("../TestImages/Honey.png")}
+            style={styles.arImage}
+          />
+        </HStack>
+        <HStack>
+          <Image
+            source={require("../TestImages/Honey.png")}
+            style={styles.arImage}
+          />
+          <Image
+            source={require("../TestImages/Honey.png")}
+            style={styles.arImage}
+          />
+        </HStack>
+      </VStack>
+      <Heading style={{ alignSelf: "center" }}>Adventurer Journal</Heading>
+    </ScrollView>
+  );
 }
 export default Photo;
