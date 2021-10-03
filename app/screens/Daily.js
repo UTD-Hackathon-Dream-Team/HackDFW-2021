55d86df39fb317ac4cdf8b34c72f4f9d25078fae
Reshmi Ranjith <rreshmiranjith@gmail.com>
2021-10-03 04:55:18 -0500

    styling

--- app/screens/Daily.js
+++ app/screens/Daily.js
@@ -9,30 +9,31 @@ import {
   VStack,
   Button,
 } from "native-base";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { getPlan } from "../util/firestore";
 
 var styles = {
   cardbg: {
     linearGradient: {
-      colors: ['#d9caa0', '#c3b590'],
+      colors: ["#d9caa0", "#c3b590"],
       start: [0, 0],
       end: [1, 0],
     },
   },
   bg: {
     linearGradient: {
-      colors: ['#f3f0e8','#cfc3a6'],
-      start: [.5, .5],
+      colors: ["#f3f0e8", "#cfc3a6"],
+      start: [0.5, 0.5],
       end: [1, 1],
     },
   },
   box: {
-    fontSize: 'md',
-    fontWeight: 'medium',
-    color: 'black',
-    textAlign: 'center',
-  }
-}
+    fontSize: "md",
+    fontWeight: "medium",
+    color: "black",
+    textAlign: "center",
+  },
+};
 
 function Daily({ navigation }) {
   let [plan, setPlan] = React.useState({});
@@ -47,15 +48,12 @@ function Daily({ navigation }) {
 
   return (
     <NativeBaseProvider config={config}>
-      <Box bg={styles.bg} style={{flex:1}}> 
-      <Heading textAlign="center" mt="3" size="lg">
-        Streak: 7 Days
-      </Heading>
-        <Box  bg={styles.cardbg} style={styles.box}
-          m="2"
-          p="7"
-          rounded="xl"
-        >
+      <Box bg={styles.bg} style={{ flex: 1 }}>
+        <Heading textAlign="center" mt="3" size="lg">
+          Streak: 7 Days
+          <MaterialCommunityIcons name="fire" size={30} color="black" />
+        </Heading>
+        <Box bg={styles.cardbg} style={styles.box} m="2" p="7" rounded="xl">
           {plan.activities && (
             <VStack space={5} alignItems="flex-start">
               {plan.activities.map((activity) => (
@@ -65,36 +63,32 @@ function Daily({ navigation }) {
               ))}
             </VStack>
           )}
-        <Button
-          onPress={() => navigation.navigate("Story")}
-          mt="6"
-          width={190}
-          size="sm"
-          style={{ alignSelf: "center" }}
-          colorScheme="light"
-        >
-          <Text fontSize="lg" color="white">Submit Daily Tasks</Text>
-        </Button>
+          <Button
+            onPress={() => navigation.navigate("Story")}
+            mt="6"
+            width={220}
+            size="sm"
+            style={{ alignSelf: "center" }}
+            colorScheme="light"
+          >
+            <Text fontSize="lg" color="white">
+              Submit Daily Tasks
+            </Text>
+          </Button>
         </Box>
 
-        <Box  bg={styles.cardbg} style={styles.box}
-          m="2"
-          p="7"
-          rounded="xl"
-        >
+        <Box bg={styles.cardbg} style={styles.box} m="2" p="7" rounded="xl">
           <Text fontSize="md">You're currently on level 2!</Text>
-          <Text fontSize="md">Help Ebony collect fruits for
-          her pie! Keep finishing your daily tasks to collect more fruit and
-          give them to her for gems!</Text>
-          
+          <Text fontSize="md">
+            Help Ebony collect fruits for her pie! Keep finishing your daily
+            tasks to collect more fruit and give them to her for gems!
+          </Text>
         </Box>
         <Image
           style={{
             height: 250,
             width: 170,
-            position: "absolute",
             alignSelf: "center",
-            top: Dimensions.get("screen").height / 2,
           }}
           source={require("../assets/bunny.png")}
         />
